ce5a3e6d3bd13c55737a17384094875bfe807cf1
tora180405 <toradigda@gmail.com>
2023-11-26 22:30:02 +0700

    add file

---
+++ angkot2/angkot2.js
@@ -0,0 +1,12 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+
+while(noAngkot <= angkotBeroperasi){
+    console.log('Angkot No. ' + noAngkot + ' Beroperasi dengan baik.');
+noAngkot++;
+}
+
+for(var noAngkot = 7; noAngkot <= jmlAngkot; noAngkot++){
+    console.log('Angkot No. ' + noAngkot + ' Sedang tidak beroperasi.');
+}
