91a4d463144dd3ccc42deb6383c49aa7c9ef0b3c
tora180405 <toradigda@gmail.com>
2023-11-27 00:04:03 +0700

    add file

---
+++ angkot3/angkot3.js
@@ -0,0 +1,11 @@
+var noAngkot = 1;
+var angkotBeroperasi = 6;
+var jumahAngkot = 10;
+
+for(noAngkot = 1; noAngkot <= 10; noAngkot++){
+    if(noAngkot <= angkotBeroperasi){
+        console.log('Angkot No. ' + noAngkot + ' Beroperasi dengan baik.');
+    }else{
+        console.log('Angkot No. ' + noAngkot + ' Sedang tidak beroperasi.');
+    }
+}
