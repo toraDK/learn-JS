c7fb8a79f977a65a791b4ddd55bedcc61f6ed0fc
tora180405 <toradigda@gmail.com>
2023-11-27 14:39:42 +0700

    edit file

--- angkot6/angkot6.js
+++ angkot6/angkot6.js
@@ -3,12 +3,9 @@ var angkotBeroperasi = 6;
 var jumahAngkot = 10;
 
 for(noAngkot = 1; noAngkot <= 10; noAngkot++){
-    if(noAngkot <= angkotBeroperasi){
-        console.log('Angkot No. ' + noAngkot + ' Beroperasi dengan baik.'); 
-        if(noAngkot === 5){
-            console.log('Angkot No. ' + noAngkot + ' Sedang lembur');
-        }
-    }else if(noAngkot === 8 || noAngkot === 10){
+    if(noAngkot <= angkotBeroperasi && noAngkot != 5){
+        console.log('Angkot No. ' + noAngkot + ' Beroperasi dengan baik.');
+    }else if(noAngkot === 8 || noAngkot === 10 || noAngkot === 5){
         console.log('Angkot No. ' + noAngkot + ' Sedang lembur');
     }else{
         console.log('Angkot No. ' + noAngkot + ' Sedang tidak beroperasi.');
