5b87390fe1388dffca009665ae9311eb4f592acf
tora180405 <toradigda@gmail.com>
2023-12-09 17:07:38 +0700

    add file

---
+++ Jumlah2VolumeKubus/jsfile.js
@@ -0,0 +1,12 @@
+var kubus1 = 8;
+var kubus2 = 3;
+var volume1;
+var volume2;
+var jumlah;
+
+volume1 = kubus1*kubus1*kubus1;
+volume2 = kubus2*kubus2*kubus2;
+
+jumlah = volume1 + volume2;
+
+console.log(jumlah);
