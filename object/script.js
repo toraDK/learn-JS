ce0d365ae23e8318b9e013470bec4a57989ddfd8
tora180405 <toradigda@gmail.com>
2023-12-11 21:05:01 +0700

    add file

---
+++ object/script.js
@@ -0,0 +1,10 @@
+var mhs = {
+    nama : "Tora Digda",
+    umur : "18",
+    ips : [0,0,0],
+    alamat : {
+        jalan : "jalan pisang kipas",
+        kota : " malang",
+        provinsi : "jawa timur"
+    }
+};
