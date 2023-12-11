87601eab6069c28beba0ba31f0eac58c67b7f677
tora180405 <toradigda@gmail.com>
2023-12-11 15:01:21 +0700

    add file

---
+++ array/script.js
@@ -0,0 +1,22 @@
+//array method
+var arr = ['tora', 'digda', 'kristiawan'];
+
+//1. method join method yang menggabungkan seluruh isi array dan menjadi string
+// console.log(arr.join('-'));
+
+// 2. push & pop
+//push untuk menambah atau  memasukkan data pada array di index terakhir
+// arr.push('anton', 'dody');
+//pop untuk menghilangkan atau menghapus data array pada index terakhir
+// arr.pop();
+// arr.pop();
+// console.log(arr.join(' - '));
+
+
+//3. unshift & shift
+//unshift menambahkan data baru pada array di index pertama
+// arr.unshift('dodit');
+
+//shift menghapus data pada awal array
+arr.shift();
+console.log(arr.join(' - '));
