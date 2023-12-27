07d652cfebc4f9f478fa3afc5e513326b219c412
tora180405 <toradigda@gmail.com>
2023-12-27 19:01:10 +0700

    edit file

--- object/script.js
+++ object/script.js
@@ -11,6 +11,7 @@
 
 //Cara untuk membuat object pada javascript
 //1. Object Literal
+//PROBLEM : tidak efektif untuk object yang banyak
 // let mahasiswa1 = {
 //     nama: 'tora',
 //     energi: 10,
@@ -30,27 +31,45 @@
 // }
 
 //2. funtion declaration
-function Mahasiswa(nama, energi) {
-    let mahasiswa = {};
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
 
-    mahasiswa.makan = function(porsi) {
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = {};
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
+
+//     mahasiswa.makan = function(porsi) {
+//         this.energi += porsi;
+//         console.log(`halo ${this.nama}, selamat makan`);
+//     }
+
+//     mahasiswa.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`halo ${this.nama}, selamat bermain`);
+//     }
+
+//     return mahasiswa;
+// }
+
+// let tora = Mahasiswa("tora",10);
+// let doddy = Mahasiswa("doddy",15);
+
+//3. Constructor Function
+function Mahasiswa(nama, energi) {
+    this.nama = nama;
+    this.energi = energi;
+
+    this.makan = function(porsi) {
         this.energi += porsi;
         console.log(`halo ${this.nama}, selamat makan`);
     }
 
-    mahasiswa.main = function (jam) {
+    this.main = function (jam) {
         this.energi -= jam;
         console.log(`halo ${this.nama}, selamat bermain`);
     }
 
-    return mahasiswa;
 }
 
-let tora = Mahasiswa("tora",10);
-let doddy = Mahasiswa("doddy",15);
-
-//3. Constructor Function
+let tora = new Mahasiswa('Tora', 10);
 
 //4. Object.create
