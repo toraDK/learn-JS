103d9d873c928c3cbc74123b3d76995f872e8d45
tora180405 <toradigda@gmail.com>
2023-12-28 21:29:05 +0700

    edit file

--- Execution Context, Hoisting, Scope/closure/script.js
+++ Execution Context, Hoisting, Scope/closure/script.js
@@ -1,7 +1,52 @@
-function init(){
-    function tampilNama(){
-        console.log(nama);
+// function init(){
+//     //let nama = 'tora';
+//     return function (nama){
+//         console.log(nama);
+//     }
+
+// }
+// let panggilNama = init();
+// panggilNama('tora');
+// panggilNama('digda');
+
+
+// function ucapkanSalam(waktu){
+//     return function(nama){
+//         console.log(`Halo ${nama}, Selamat ${waktu}, semogaharimu menyenangkan!`)
+//     }
+// }
+
+// let selamatPagi = ucapkanSalam('pagi');
+// let selamatSiang = ucapkanSalam('Siang');
+// let selamatMalam = ucapkanSalam('Malam');
+
+// selamatPagi('tora');
+// selamatMalam('digda');
+
+// // console.dir(selamatMalam);
+
+
+// let add = function (){
+//     let counter = 0;
+//     return function (){
+//         ++counter;
+//     }
+// }
+
+// let a = add();
+
+// console.log(a());
+// console.log(a());
+// console.log(a());
+
+
+let add = (function (){
+    let counter = 0;
+    return function (){
+        ++counter;
     }
-    tampilNama();
-}
-init();
+})();
+
+console.log(add());
+console.log(add());
+console.log(add());
