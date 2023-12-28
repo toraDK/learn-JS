a04869a80fb982530d0e92dab4d9b541b91136af
tora180405 <toradigda@gmail.com>
2023-12-28 20:26:06 +0700

    edit file

---
+++ Execution Context, Hoisting, Scope/script.js
@@ -0,0 +1,9 @@
+//2.1 EXECUTION CONTEXT, HOISTING & SCOPE
+
+console.log(nama);
+var nama = 'tora';
+
+//creation phase pada Global Context
+//nama var = undefined
+//nama function = fn()
+//hoisting
