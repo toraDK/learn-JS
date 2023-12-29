cd40165068561518b94ee8d8e9a8cbacbc5b7a28
tora180405 <toradigda@gmail.com>
2023-12-29 19:34:04 +0700

    edit file

--- function/script.js
+++ function/script.js
@@ -20,13 +20,93 @@
 // console.log(tampilNama());
 
 
-let mahasiswa = ['Tora Digda Kristiawan', 'PPPPPP', 'AAAAAAA'];
+// let mahasiswa = ['Tora Digda Kristiawan', 'PPPPPP', 'AAAAAAA'];
 
 // let jumlahHuruf = mahasiswa.map(function (nama){
 //     return nama.length;
 // });
 // console.log(jumlahHuruf);
 
-let jumlahHuruf = mahasiswa.map(nama => ({nama: nama, jmlHuruf: nama.length}));
-console.log(jumlahHuruf);
-// console.table(jumlahHuruf);
+// let jumlahHuruf = mahasiswa.map(nama => ({nama: nama, jmlHuruf: nama.length}));
+// console.log(jumlahHuruf);
+// // console.table(jumlahHuruf);
+
+
+
+
+
+//================================this pada arrow=========================
+
+
+//CONSTRUCTOR FUNCTION
+// const mahasiswa = function (){
+//     this.nama = 'tora';
+//     this.umur = 18;
+//     this.sayHello = function(){
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`)
+//     }
+// }
+
+// const tora = new mahasiswa();
+
+
+
+
+
+
+
+//Arrow function
+// const mahasiswa = function (){
+//     this.nama = 'tora';
+//     this.umur = 18;
+//     this.sayHello = function(){
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`)
+//     }
+// }
+
+// const tora = new mahasiswa();
+
+
+
+
+
+//Object literal
+const mhs1 = {
+    nama: 'tora',
+    umur: 18,
+    sayHello: () => {
+        // console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun`)
+        console.log(this);
+    }
+}
+
+
+// const mahasiswa = function (){
+//     this.nama = 'tora';
+//     this.umur = 18;
+//     this.sayHello = function(){
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`)
+//     }
+// }
+
+// const tora = new mahasiswa();
+
+
+const mahasiswa = function (){
+    this.nama = 'tora';
+    this.umur = 18;
+    this.sayHello = function(){
+        console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`)
+    }
+
+    setInterval(() =>{
+        console.log(this.umur++);
+    }, 500);
+
+    //hasil fungsi interval dibawah berbeda dengan fungsi interval di atas
+    setInterval(function (){
+        console.log(this.umur++);
+    }, 500);
+}
+
+const tora = new mahasiswa();
