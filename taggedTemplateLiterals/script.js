9d68a672ddd2fe6328a979166bc6f59cfc4df76c
tora180405 <toradigda@gmail.com>
2024-01-01 22:44:03 +0700

    edit file

--- taggedTemplateLiterals/script.js
+++ taggedTemplateLiterals/script.js
@@ -1,14 +1,34 @@
-//Tagged Templates
+// //Tagged Templates
+// const nama = 'Tora Digda Kristiawan';
+// const umur = 18;
+
+// function coba(strings, ...args){
+//     // let result = '';
+//     // strings.forEach((str, i) => {
+//     //     result += `${str}${args[i] || ''}`;
+//     // });
+//     // return result;
+
+//     return strings.reduce((result, str, i) => `${result}${str}${args[i] || ''}`, '');
+// }
+
+// const str = coba`Halo, nama saya ${nama}, saya ${umur} tahun.`;
+// console.log(str);
+
+
+
+
+
+
+
+//Highligt
 const nama = 'Tora Digda Kristiawan';
 const umur = 18;
 
-function coba(strings, ...args){
-    let result = '';
-    strings.forEach((str, i) => {
-        result += `${str}${args[i] || ''}`;
-    });
-    return result;
+function Highligt(strings, ...args){
+    return strings.reduce((result, str, i) => `${result}${str}<span class="h1">${args[i] || ''}</span>`, '');
 }
 
-const str = coba`Halo, nama saya ${nama}, saya ${umur} tahun.`;
-console.log(str);
+const str = Highligt`Halo, nama saya ${nama}, saya ${umur} tahun.`;
+
+document.body.innerHTML = str;
